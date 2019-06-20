5385976998c8a967b52fc1ab14ae26f04da28a78
svelezg <47928168+svelezg@users.noreply.github.com>
2019-06-20 19:29:00 +0000

    10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+	/*
+	* This is the preferred style for multi-line
+	* comments in C source code.
+	* Please use it consistently.
+	* Positive anything is better than negative nothing.
+	*/
+
+/**
+ * main - Entry point
+ *
+ * putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k;
+	int i;
+	int j;
+
+	for (k = 48; k < 58; k++)
+		{
+		for (i = 48; i < 58; i++)
+			{
+			for (j = i; j < i + 1; j++)
+				{
+				putchar(k);
+				putchar(j);
+
+				if (!(j == 57 && k == 57))
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+
+	putchar('\n');
+
+	return (0);
+}
