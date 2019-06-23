bc85124591fc1ca7aa176003870a4f928fc4972a
svelezg <47928168+svelezg@users.noreply.github.com>
2019-06-23 02:17:22 +0000

    101-quote.c (fixed styling and spaces)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,11 @@ int main(void)
 {
 int size;
 
-size = sizeof ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+size = sizeof(
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", size);
+write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", size);
 
 return (1);
 }
