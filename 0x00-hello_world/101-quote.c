ff72085ff2092b26a5d7cbe05b7a9ca8a6b03624
svelezg <47928168+svelezg@users.noreply.github.com>
2019-06-23 02:10:42 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints message
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+int size;
+
+size = sizeof ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", size);
+
+return (1);
+}
