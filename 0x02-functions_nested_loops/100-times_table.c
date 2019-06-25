0d96b3b25bfa316960d27fe3860c8a69af6aa650
svelezg <47928168+svelezg@users.noreply.github.com>
2019-06-25 21:01:38 +0000

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * @n: Void input
+ *
+ */
+void print_times_table(int n)
+{
+
+	/* local variable declaration */
+	int i;
+	int j;
+
+	for (i = 0 ; i <= n ; i++)
+		{
+		if (n > 15 || n < 0)
+			break;
+		for (j = 0 ; j <= n ; j++)
+			{
+			int p = i * j;
+			int c = p / 100;
+			int d = (p % 100) / 10;
+			int u = p % 10;
+
+			if (c > 0)
+				_putchar('0' + c);
+			else
+				_putchar(' ');
+
+			if (d > 0 || c > 0)
+				_putchar('0' + d);
+			else
+				_putchar(' ');
+
+			if (u > 0 || d > 0 || c > 0)
+				_putchar('0' + u);
+			else
+				_putchar('0');
+
+			if (j != n)
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
+			else
+				_putchar('\n');
+			}
+		}
+}
