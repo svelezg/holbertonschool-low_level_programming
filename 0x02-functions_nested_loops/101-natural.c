b35c27e3e13a91e1b06f9f158ab36d1dae4749ba
svelezg <47928168+svelezg@users.noreply.github.com>
2019-06-26 00:33:33 +0000

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 3;
+	int sum = 0;
+
+	do {
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+
+		i++;
+	} while (i < 1023);
+
+	printf("%i\n", sum);
+	return (0);
+}
