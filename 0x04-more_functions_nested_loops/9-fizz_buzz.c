ae7fa90ad21f5d919296df64c3f4d22aa1133184
svelezg <47928168+svelezg@users.noreply.github.com>
2019-06-27 17:15:18 +0000

    9-fizz_buzz.c (trail space and format)

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,8 @@
 
 /**
  * main -  prints the numbers from 1 to 100.
- * But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz.
+ * But for multiples of 3 print Fizz instead of the number
+ * and for the multiples of 5 print Buzz.
  * For numbers which are multiples of both three and five print FizzBuzz
  *
  * Return: Always 0.
@@ -13,7 +14,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1 ; i <= 100 ; i++)
+	for (i = 1 ; i <= 99 ; i++)
 		{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
@@ -28,7 +29,7 @@ int main(void)
 			printf("%d ", i);
 		}
 
-	printf("\n");
+	printf("Buzz\n");
 
-    return (0);
+	return (0);
 }
