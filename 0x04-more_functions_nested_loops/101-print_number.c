f19c181fb047402187a309b6b3e83b90d477ff65
svelezg <47928168+svelezg@users.noreply.github.com>
2019-06-28 01:47:21 +0000

    101-print_number.c (unsigned int)

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,17 +9,21 @@
  */
 void print_number(int n)
 {
-	unsigned int d, i;
-	unsigned int p = 0;
-	unsigned int j = 1;
+	int i, p;
+	unsigned int z, d, j;
+
+	p = 0;
+	j = 1;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		z = -n;
 	}
+	else
+		z = n;
+	d = z;
 
-	d = n;
 	while (d != 0)
 		{
 		d = d / 10;
@@ -30,10 +34,10 @@ void print_number(int n)
 
 	for (i = 0 ; i < (p - 1) ; i++)
 		{
-		n = n % j;
+		z = z % j;
 		j = j / 10;
-		_putchar('0' + (n / j));
+		_putchar('0' + (z / j));
 		}
 
-	_putchar('0' + (n % 10));
+	_putchar('0' + (z % 10));
 }
