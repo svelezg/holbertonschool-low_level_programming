a196cd9debc55e6348c87d41d27099712547afc4
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-02 14:57:17 +0000

    9-strcpy.c (empty buffer)

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,14 +13,15 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-/*	while ((src[i] = dest[i]) != ('\0')) */
-/*		i++; */
-
-
-
 	while (src[i] != ('\0'))
 	{
 	*(dest + i) = *(src + i);
+	i++;
+	}
+
+	while (dest[i] != ('\0'))
+	{
+	*(dest + i) = '\0';
 	i++;
 	}
 
