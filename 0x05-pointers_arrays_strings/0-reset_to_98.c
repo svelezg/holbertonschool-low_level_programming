5df71763721197c7fc3ca008ac0f0984a7567518
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-02 12:20:09 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 -  updates the value it points to to 98.
+ *
+ * @n: The pointer
+ * Return: 0.
+ *
+ */
+void reset_to_98(int *n)
+{
+	/* local variable declaration */
+
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,54 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+char *_strcpy(char *dest, char *src);
+
+void print_array(int *a, int n);
+
+void puts_half(char *str);
+
+void puts2(char *str);
+
+void rev_string(char *s);
+
+void print_rev(char *s);
+
+void _puts(char *str);
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+#endif /* _HOLBERTON_H_ */
