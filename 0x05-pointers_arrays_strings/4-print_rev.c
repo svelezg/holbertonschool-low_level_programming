adaefa69433170df76349b209409b7d3cf03e288
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-02 12:30:42 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line, to stdout.
+ *
+ * @s: string
+ * Return: void.
+ *
+ */
+void print_rev(char *s)
+
+{
+	/* local variable declaration */
+	int l, a;
+
+	l = 0;
+
+	while (s[l] != ('\0'))
+	{
+	l++;
+	}
+
+	while (l >= 0)
+	{
+	a = s[l];
+	_putchar(a);
+	l--;
+	}
+
+
+	 _putchar('\n');
+
+}
