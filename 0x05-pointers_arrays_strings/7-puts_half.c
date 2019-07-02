a98ccdd4f4ec8ea1ccf5b91f0cdef936773df13d
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-02 12:36:20 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * puts_half -  prints half of a string.
+ *
+ * @str: string
+ * Return: void.
+ *
+ */
+void puts_half(char *str)
+
+{
+	/* local variable declaration */
+	int l, i;
+	char a;
+
+	l = 0;
+
+	while (str[l] != ('\0'))
+	{
+	l++;
+	}
+
+	if (l % 2 == 0)
+		{
+		for (i = l / 2 ; i < l ; i++)
+			{
+			a = *(str + i);
+			_putchar(a);
+			}
+		}
+	else
+		for (i = (l - 1) / 2 ; i < l ; i++)
+			{
+			a = *(str + i);
+			_putchar(a);
+			}
+
+	_putchar('\n');
+}
