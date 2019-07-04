33ff2cacea847749432aa1097b9cb98fb43536a3
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-04 17:32:26 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * _strncpy -  copies n characters of a string.
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: n bytes to use
+ * Return: concatenated string.
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	/* local variable declaration */
+	int i, j;
+	char *buff;
+
+	buff = dest;
+
+	for (j = 0 ; src[j] != '\0'; j++)
+		;
+
+	if (n < j)
+		{
+		for (i = 0; i < n; i++)
+			{
+			if (src[i] != '\0')
+				dest[i] = src[i];
+			else if (dest[i] != '\0')
+				{
+				;
+				}
+			}
+		}
+	else
+		{
+		while (src[i] != '\0')
+			{
+			dest[i] = src[i];
+			i++;
+			}
+			dest[i] = '\0';
+		}
+	return (buff);
+}
