68f907481ccbbbe12e33d0124e16d39230b1edfc
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-04 20:30:22 +0000

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * cap_string -  capitalizes all words of a string
+ * @a: string
+ * Return: string
+ *
+ */
+char *cap_string(char *a)
+
+{
+	/* local variable declaration */
+	int i;
+	char m;
+	char b[999];
+
+	i = 0;
+	while (a[i] != '\0')
+		{
+		m = a[i];
+		if ((m >= 64 && m <= 90) || !(m >= 97 && m <= 122))
+			b[i] = m;
+		else
+			{
+			if (a[i - 1] == ' ' || a[i - 1] == '\n')
+				m = m - ('a' - 'A');
+			b[i] = m;
+			}
+		i++;
+		}
+
+	i = 0;
+	while (a[i] != '\0')
+		{
+		a[i] = b[i];
+		i++;
+		}
+
+	return (a);
+
+}
