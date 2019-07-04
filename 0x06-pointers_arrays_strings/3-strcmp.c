5cf14aa91348f0920b502f759efa6861b1108fdc
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-04 18:59:09 +0000

    3-strcmp.c (n = diffference)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,18 +16,9 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' || s2[i] != '\0')
 		{
-		if (s1[i] == s2[i])
-			n = 0;
-		else if (s1[i] > s2[i] || s2[i] == '\0')
-			{
-			n = 15;
+		n = s1[i] - s2[i];
+		if (n != 0 || s2[i] == '\0')
 			break;
-			}
-		else
-			{
-			n = -15;
-			break;
-			}
 		i++;
 		}
 
