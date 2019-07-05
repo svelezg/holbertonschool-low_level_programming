3d7f2e9e166654d4c60c37ea784eec9300a68a51
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-05 02:47:59 +0000

    100-print_number.c (recursion)

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -2,7 +2,7 @@
 
 /**
  * print_number -  prints an integer
- * @n: string
+ * @n: intiger
  * Return: void
  *
  */
@@ -10,38 +10,23 @@ void print_number(int n)
 
 {
 	/* local variable declaration */
-	int i, p;
-	unsigned int z, d, j;
-
-	p = 0;
-	j = 1;
-
+	unsigned int z;
 
 	if (n < 0)
-	{
+		{
 		_putchar('-');
 		z = -n;
-	}
+		}
 	else
 		z = n;
-	d = z;
 
-	while (d != 0)
+	if (z >= 10)
 		{
-		d = d / 10;
-		j = j * 10;
-		p++;
-	}
-
-
-	for (i = 0 ; i < (p - 1) ; i++)
-		{
-		z = z % j;
-		j = j / 10;
-		_putchar('0' + (z / j));
+		print_number(z / 10);
+		_putchar('0' + (z % 10));
 		}
-
-	_putchar('0' + (z % 10));
+	else
+		_putchar('0' + (z % 10));
 
 
 }
