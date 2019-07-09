7ec14c6bfb00aa2b9dcd96d04681b43f68ccaee1
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-09 02:48:56 +0000

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,23 +11,30 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	/* local variable declaration */
-	unsigned int i, j, k;
+	int i, j, k, l;
 	unsigned int res = 0;
 
 	k = 0;
 	while (*(accept + k) != '\0')
 		k++;
 
-	for (i = 0 ; i <= k ; i++)
+	l = 0;
+	while (*(s + l) != '\0')
+		l++;
+
+	for (i = 0 ; i < l ; i++)
+	{
+		for (j = 0 ; j <= k ; j++)
 		{
-		j = 0;
-		while (*(accept + j) != '\0')
+/*			printf("s[%d]=%c accept[%d]=%c\n",i, s[i], j, accept[j]);*/
+			if (*(s + i) == *(accept + j))
 			{
-/*			printf("s[%d]=%c accept[%d]=%c",i, s[i], j, accept[j]);*/
-			if (*(accept + j) == *(s + i))
 				res++;
-			j++;
+				break;
 			}
+			if (*(accept + j) == '\0')
+				return (res);
 		}
+	}
 	return (res);
 }
