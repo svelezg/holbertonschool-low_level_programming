247673d861e52ff4b051f3e88ec541e97e1aaa56
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-09 03:32:13 +0000

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-/* #include <stdio.h> */
 
 /**
  * _strstr -  Locates a substring.
@@ -13,30 +12,28 @@
 char *_strstr(char *haystack, char *needle)
 {
 	/* local variable declaration */
-	unsigned int i, j, k;
+	unsigned int i, j, k, l;
 
 	k = 0;
 	while (*(needle + k) != '\0')
 		k++;
 
-	i = 0;
-	while (*(haystack + i) != '\0')
+	l = 0;
+	while (*(haystack + l) != '\0')
+		l++;
+
+	for (i = 0 ; i < l ; i++)
+	{
+		for (j = 0 ; j < k ; j++)
 		{
-		j = 0;
-		while (*(needle + j) != '\0')
-			{
-/*			printf("haystack[%d]=%c needle[%d]=%c\n",i, haystack[i], j, needle[j]);*/
-			if (*(needle + j) == *(haystack + i))
-				{
-				j++;
-				i++;
-				}
-			else
+			if (*(needle + j) != *(haystack + i + j))
 				break;
-			if (j == k)
-				return (haystack + i - j);
-			}
-		i++;
+			if (j == k - 1)
+				return (haystack + i);
+			if (i + j == l - 1)
+				return (0);
 		}
+
+	}
 	return (0);
 }
