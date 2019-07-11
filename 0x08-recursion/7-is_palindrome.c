c4e9cb6b34f38cf1a08d8a20e1bc317786a539d7
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-11 03:15:44 +0000

    7-is_palindrome.c

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * _lenght - Returns 1 if the input integer is a prime number,
+ *
+ * @s: string
+ * @i: increment
+ * Return: value.
+ *
+ */
+int _lenght(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _lenght(++s));
+}
+
+/**
+ * palindrome - returns 1 if a string is a palindrome and 0 if not
+ *
+ * @s: string
+ * @i: counter
+ * Return: 0 or 1.
+ *
+ */
+
+int palindrome(int i, char *s)
+{
+
+	if (*s == '\0')
+		return (1);
+
+	if (*s != *(s + _lenght(s) - i))
+		return (0);
+	else
+		return (palindrome(++i, ++s));
+}
+
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ *
+ * @s: string
+ * Return: 1 for yes 0 for no.
+ *
+ */
+int is_palindrome(char *s)
+{
+	return (palindrome(1, s));
+}
