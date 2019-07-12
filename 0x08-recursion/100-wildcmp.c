cb34a36b030761dcda49575d27c5170a96cd86fb
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-12 03:09:24 +0000

    100-wildcmp.c (more cases)

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,6 +1,43 @@
 #include "holberton.h"
 
 /**
+ * wild - compares two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * @a: asterisk masker
+ * Return: 0 or 1.
+ *
+ */
+
+int wild(char *s1, char *s2, int a)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	else if (*s1 == '\0' && *s2 == '*' && *++s2 == '\0')
+		return (1);
+
+	else if (*s1 == *s2)
+		return (wild(++s1, ++s2, 0));
+
+	else
+	{
+		if (*s2 == '*')
+		{
+			return (wild(s1, ++s2, 1));
+		}
+		else if (a == 1)
+		{
+			return (wild(++s1, s2, 1));
+		}
+		else
+			return (0);
+	}
+}
+
+
+/**
  * wildcmp - compares two strings
  *
  * @s1: string 1
@@ -11,14 +48,5 @@
 
 int wildcmp(char *s1, char *s2)
 {
-
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s1 != *s2)
-		return (0);
-
-	else
-		return (wildcmp(++s1, ++s2));
+	return (wild(s1, s2, 0));
 }
-
