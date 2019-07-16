8c923eb1137aabaffd5ef828e45a08fd5456d9ce
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-16 18:44:28 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,12 +18,12 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	char *array = (char *) malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
 
 	for (i = 0 ; i < size ; i++)
 	{
-	*(array + i) = c;
+		*(array + i) = c;
 	}
-
-
 	return (array);
 }
