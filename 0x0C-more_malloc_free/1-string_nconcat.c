040d7676ba48db49e46efcd7308d01ba6f4c46dd
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-18 18:51:51 +0000

    1-string_nconcat.c (change notation)

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,16 +11,16 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1 = 0, l2 = 0, i = 0, j = 0;
+	unsigned int l1 = 0, l2 = 0, i, j;
 	char *string;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (*(s1 + l1) != '\0')
+	while (s1[l1] != '\0')
 		l1++;
-	while (*(s1 + l2) != '\0')
+	while (s2[l2] != '\0')
 		l2++;
 	if (n >= l2)
 		n = l2;
@@ -29,14 +29,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0 ; i < l1 ; i++)
-		*(string + i) = *(s1 + i);
+		string[i] = s1[i];
 
 	for (j = 0 ; j < n; j++)
 	{
-		*(string + i) = *(s2 + j);
+		string[i] = s2[j];
 		i++;
 	}
-	*(string + i) = '\0';
+	string[i] = '\0';
 
 	return (string);
 }
