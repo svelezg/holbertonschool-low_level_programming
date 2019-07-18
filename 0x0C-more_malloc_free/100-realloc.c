d04c31a946c0ebba92c6eaa4dd7ea2a2fef5bf0a
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-18 19:48:02 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,42 +12,25 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-	char *buff;
 	char *newptr;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-		ptr = malloc(new_size);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size > old_size)
-	{
-		buff = malloc(old_size);
-		i = 0;
-		while (i < old_size)
-		{
-			*(buff + i) = *((char *)ptr + i);
-			i++;
-		}
 
-		newptr = (char *)malloc(new_size);
-		if (newptr == NULL)
-			return (ptr);
-		if (newptr != NULL)
-		{
-			i = 0;
-			while (i < old_size)
-			{
-				*(newptr + i) = *(buff + i);
-				i++;
-			}
-			free(buff);
-			ptr = newptr;
-		}
-	}
-	return (ptr);
+	if (new_size == old_size)
+		return (ptr);
+
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+		return (ptr);
+
+	for (i = 0 ; i < old_size ; i++)
+		*(newptr + i) = *((char *)ptr + i);
+
+	free(ptr);
+	return (newptr);
 }
