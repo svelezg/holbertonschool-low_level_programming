3840df46efcddebfa0e0fcc29c736574db17daeb
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-18 20:08:03 +0000

    100-realloc.c (conditions order)

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,13 +11,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i, size;
+	unsigned int i, min_size;
 	char *newptr;
-
-	if (old_size > new_size)
-		size = new_size;
-	else
-		size = old_size;
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -28,11 +23,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
+	if (old_size > new_size)
+		min_size = new_size;
+	else
+		min_size = old_size;
+
 	newptr = malloc(new_size);
 	if (newptr == NULL)
 		return (ptr);
 
-	for (i = 0 ; i < size ; i++)
+	for (i = 0 ; i < min_size ; i++)
 		*(newptr + i) = *((char *)ptr + i);
 
 	free(ptr);
