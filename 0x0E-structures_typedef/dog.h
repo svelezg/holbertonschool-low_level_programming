3aa712e2efa5e0948cd4812cb873fe3e28d4d754
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-22 14:32:23 +0000

    directory, README.md, dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Dog structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: Structure fir Dog information including:
+ * name, age and owner
+ */
+struct dog {
+name, type = char *
+age, type = float
+owner, type = char *
+};
