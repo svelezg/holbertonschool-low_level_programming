87cf7a9605cd16db5587b9fc6c61e1f216f5e4db
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-22 19:53:00 +0000

    4-new_dog.c (sizeof strings)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,15 +11,13 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int l_name, l_owner, i, j;
-	dog_t new_dog, *ptr;
+	int l_name = 0, l_owner = 0, i, j;
+	dog_t *ptr;
 	char *n_name, *n_owner;
 
-	l_name = 0;
 	while (*(name + l_name) != '\0')
 		l_name++;
 
-	l_owner = 0;
 	while (*(owner + l_owner) != '\0')
 		l_owner++;
 
@@ -27,7 +25,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ptr == NULL)
 		return (NULL);
 
-	n_name = malloc(sizeof(char) * l_name);
+	n_name = malloc(sizeof(char) * (1 + l_name));
 	if (n_name == NULL)
 	{
 		free(ptr);
@@ -35,7 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	n_owner = malloc(sizeof(char) * l_owner);
+	n_owner = malloc(sizeof(char) * (1 + l_owner));
 	if (n_owner == NULL)
 	{
 		free(ptr);
@@ -44,18 +42,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (i = 0 ; i < l_name ; i++)
+	for (i = 0 ; i <= l_name ; i++)
 		*(n_name + i) = *(name + i);
 /*	k = sizeof(n_name);*/
 /*	printf("%s  %d\n", n_name, k);*/
 
-	for (j = 0 ; j < l_owner ; j++)
+	for (j = 0 ; j <= l_owner ; j++)
 		*(n_owner + j) = *(owner + j);
 /*	printf("%s\n", n_owner);*/
 
-	new_dog.name = n_name;
-	new_dog.age = age;
-	new_dog.owner = n_owner;
-	ptr = &new_dog;
+	ptr->name = n_name;
+	ptr->age = age;
+	ptr->owner = n_owner;
 	return (ptr);
 }
