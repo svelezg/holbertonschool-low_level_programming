cf7abb4b857d14b824b640962023ebfc89b1c922
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-01 18:44:17 +0000

    corrected end character

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,13 +11,14 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int len = 0;
+	unsigned int len;
 	list_t *new_node;
 
 	if (head == NULL)
 		return (NULL);
 
-	while (*(str + len) != '0')
+	len = 0;
+	while (*(str + len) != '\0')
 		len++;
 
 	/* 1. allocate node */
