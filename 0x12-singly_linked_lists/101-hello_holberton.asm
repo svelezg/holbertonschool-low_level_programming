61ab69b5627caecba9c0a75aa81df05a7767108e
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-01 21:27:27 +0000

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,16 @@
-section .data
-       format db "%s", 10, 0
-       msg1 db "Hello, Holberton", 0
+section .rodata
+    msg:    db 'Hello, Holberton', 10
+    msglen: equ $-msg
+
 section .text
-extern printf
-global main
+        global main
+
 main:
-       mov  esi, msg1
-       mov  edi, format
-       mov  eax, 0     ; no f.p. args
-       call printf
-       mov ebx, 0      ; normal-exit code
-       mov eax, 1      ; process-termination service
-       int 0x80        ; linux kernel service
+        mov rdi, 1			; write(1, msg, msglen)
+        mov rsi, msg
+        mov rdx, msglen
+        mov rax, 1
+        syscall				; exit(0)
+        mov rdi, 0
+        mov rax, 60
+        syscall
