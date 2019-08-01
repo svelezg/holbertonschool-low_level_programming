07a8360f6abff95194c245bb02816762ee5162a6
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-01 20:27:36 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+
+/**
+ * startupfun - Executes after main
+ */
+void startupfun(void)
+{
+	char *str1;
+	char *str2;
+
+	str1 = "You're beat! and yet, you must allow,\nI bore my house upon";
+	str2 = " my back!\nStartup method before main()\n";
+
+	printf("%s%s", str1, str2);
+
+}
