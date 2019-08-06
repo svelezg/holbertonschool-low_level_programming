1ce5b3ad536e72376e08e4da00113cfd34f2b787
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-06 20:07:14 +0000

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * free_listint_safe - Free lists with a loop.
+ * @h: Head pointer
+ * Return: number of elements.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	size_t i;
+	listint_t *next;
+	listint_t *array[500];
+
+	if (h == NULL || *h == NULL)
+		exit(98);
+
+	while (*h)
+	{
+		i = 0;
+		while (i < size)
+		{
+/*			printf("addres [%lu] =[%p]\n", i, (void *)array[i]);*/
+			if (array[i] == *h)
+			{
+/*				printf("-> [%p] %i\n", (void *)head, head->n);*/
+				*h = NULL;
+				return (size);
+			}
+			i++;
+		}
+
+		array[size] = *h;
+
+		next = (*h)->next;
+		(*h)->next = NULL;
+		free(*h);
+		*h = next;
+		size++;
+	}
+
+
+	*h = NULL;
+	h = NULL;
+
+	return (size);
+}
