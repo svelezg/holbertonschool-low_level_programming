dbc0a526f4fd4a2c5ce8cd2ebab92b7acb42a46f
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-13 17:28:50 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include "holberton.h"
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #include <unistd.h>
-
 #include <stdlib.h>
-
 
 /**
  * read_textfile - Reads a text file and prints it to the POSIX
@@ -36,15 +32,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	res_read = read(fd, buf, letters);
 	if (res_read <= 0)
+	{
+		free(buf);
+		close(fd);
 		return (0);
+	}
+
+	free(buf);
+	close(fd);
 
 	res_write = write(1, buf, res_read);
 
-	free(buf);
-
-	close(fd);
-
-	if (res_write < 0 || res_read != res_write)
+	if (res_write <= 0 || res_read != res_write)
 		return (0);
 
 	return (res_write);
