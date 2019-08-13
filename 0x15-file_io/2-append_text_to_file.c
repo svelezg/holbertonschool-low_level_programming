cbb510b0c827c83dcc27ce3ff8cb6d9432de4c29
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-13 18:48:24 +0000

    2-append_text_to_file.c (conditions order)

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,30 +21,25 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, res_write;
 
+
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
-
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		return (1);
-	}
-	else
-	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
+			return (-1);
+
 		res_write = write(fd, text_content, sizeof(text_content));
 		if (res_write == -1)
 		{
+			close(fd);
 			write(STDOUT_FILENO, "fails", 6);
 			return (-1);
 		}
 	}
 
 	close(fd);
-
 	return (1);
 }
