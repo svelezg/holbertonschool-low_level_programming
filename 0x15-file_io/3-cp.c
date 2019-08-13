e470f265b5d0eb37ac56998bbe39f0fe52986753
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-13 22:41:29 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,76 +6,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
 /**
- * read_file - Read file.
- * @file_from: Name of the source file.
- * @buf: writing buffer
- * Return: 1 on success, -1 on failure.
+ * _error - Read file.
+ * @e: Error number
+ * @s: string
+ * @fd: integer
  */
-int read_file(const char *file_from, char *buf)
+void _error(int e, const char *s, int fd)
 {
-	int fd_read, res_read, cl_read;
-
-	fd_read = open(file_from, O_RDONLY);
-	if (fd_read == -1)
-		return (-1);
-
-	res_read = read(fd_read, buf, 1024);
-
-	if (res_read == -1)
-		return (-1);
-
-	cl_read = close(fd_read);
-
-	if (cl_read == -1)
+	if (e == 98)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", fd_read);
+		dprintf(2, "Error: Can't read from file %s\n", s);
+		exit(98);
+	}
+	if (e == 99)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", s);
+		exit(99);
+	}
+	if (e == 100)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", fd);
 		exit(100);
 	}
-
-	return (res_read);
 }
-
-/**
- * write_file - Read file.
- * @file_to: Name of the source file.
- * @buf: writing buffer
- * @res_read: characters to read
- * Return: 1 on success, -1 on failure.
- */
-int write_file(const char *file_to, char *buf, int res_read)
-{
-	int fd_write, res_write, cl_write, lenght;
-
-	if (buf != NULL)
-	{
-		fd_write = open(file_to, O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
-		if (fd_write == -1)
-			return (-1);
-
-		lenght = 0;
-		while (*(buf + lenght) != '\0')
-			lenght++;
-
-		res_write = write(fd_write, buf, lenght);
-
-		if (res_write == -1 || res_read != res_write)
-			return (-1);
-	}
-
-
-	cl_write = close(fd_write);
-
-	if (cl_write == -1)
-	{
-		dprintf(2, "Error: Can't close fd %i\n", fd_write);
-		exit(100);
-	}
-
-	return (res_write);
-}
-
 
 /**
  * cp - Copies the content of a file to another file.
@@ -85,11 +39,9 @@ int write_file(const char *file_to, char *buf, int res_read)
  */
 int cp(const char *file_from, const char *file_to)
 {
-	/*87*/
-
+	int fd_read, res_read, cl_read;
+	int fd_write, res_write, cl_write, lenght;
 	char *buf;
-	int res_read = 0;
-	int res_write = 0;
 	int res_cp = 0;
 
 	do {
@@ -97,30 +49,46 @@ int cp(const char *file_from, const char *file_to)
 		if (buf == NULL)
 			return (-1);
 
-		res_read = read_file(file_from, buf);
+		/* READ */
+		fd_read = open(file_from, O_RDONLY);
+		if (fd_read == -1)
+			_error(98, file_from, fd_read);
+
+		res_read = read(fd_read, buf, 1024);
+
 		if (res_read == -1)
-		{
-			free(buf);
-			dprintf(2, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
+			_error(98, file_from, fd_read);
 
-		res_write = write_file(file_to, buf, res_read);
-		if (res_write == -1)
+		/* Write */
+		if (buf != NULL)
 		{
-			free(buf);
-			dprintf(2, "Error: Can't write to file %s\n", file_to);
-			exit(99);
-		}
+			fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+			if (fd_write == -1)
+				_error(99, file_to, fd_write);
 
+			lenght = 0;
+			while (*(buf + lenght) != '\0')
+				lenght++;
+
+			res_write = write(fd_write, buf, lenght);
+
+			if (res_write == -1 || res_read != res_write)
+				_error(99, file_to, fd_write);
+		}
 		free(buf);
-
 		res_cp += res_write;
-	} while (res_read > 1024);
+	} while (res_write == 1024);
+
+	cl_write = close(fd_write);
+	if (cl_write == -1)
+		_error(100, file_to, fd_write);
+
+	cl_read = close(fd_read);
+	if (cl_read == -1)
+		_error(100, file_from, fd_read);
 
 	return (res_cp);
 }
-
 
 /**
  * main - Copies the content of a file to another file.
@@ -137,13 +105,11 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: cp file_from file_to");
 		exit(97);
 	}
-
 	if (av[1] == NULL || av[2] == NULL)
 		return (-1);
 
 	res = cp(av[1], av[2]);
 	if (res == -1)
 		return (-1);
-
 	return (0);
 }
