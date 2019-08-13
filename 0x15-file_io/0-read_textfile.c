995d434ef2956429592c1a1ad81ead0cfca5ee6a
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-13 16:58:52 +0000

    Task 0 to 2

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "holberton.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <stdlib.h>
+
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX
+ * standard output.
+ * @filename: file.
+ * @letters: Number of letters it should read and print.
+ * Return: Actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t res_read, res_write;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	res_read = read(fd, buf, letters);
+	res_write = write(1, buf, res_read);
+
+	free(buf);
+
+	close(fd);
+
+	if (res_read != res_write)
+		return (0);
+
+	return (res_write);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "holberton.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <stdlib.h>
+
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * standard output.
+ * @filename: Name of the file to append.
+ * @text_content: NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t res_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		res_write = write(fd, text_content, sizeof(text_content));
+		if (res_write == -1)
+		{
+			write(1, "fails", 6);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
