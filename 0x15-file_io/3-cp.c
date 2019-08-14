7b7adcacc92671b83f57b382204d16f5ffc5af50
svelezg <47928168+svelezg@users.noreply.github.com>
2019-08-14 00:50:32 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,29 +9,19 @@
 /**
  * _error - Read file.
  * @e: Error number
- * @buf: buffer
- * @s: string
- * @fd: integer
+ * @filename: File name
  */
-void _error(int e, char *buf, const char *s, int fd)
+void _error(int e, char *filename)
 {
 	if (e == 98)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", s);
-		free(buf);
+		dprintf(2, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
 	if (e == 99)
 	{
-		dprintf(2, "Error: Can't write to %s\n", s);
-		free(buf);
+		dprintf(2, "Error: Can't write to %s\n", filename);
 		exit(99);
-		close(fd);
-	}
-	if (e == 100)
-	{
-		dprintf(2, "Error: Can't close fd %i\n", fd);
-		exit(100);
 	}
 }
 
@@ -41,48 +31,47 @@ void _error(int e, char *buf, const char *s, int fd)
  * @file_to: Name of the destination file.
  * Return: 1 on success, -1 on failure.
  */
-void cp(const char *file_from, const char *file_to)
+void cp(char *file_from, char *file_to)
 {
-	int fd_read, res_read, cl_read;
-	int fd_write, res_write, cl_write, lenght;
-	char *buf;
+	int fd_read, res_read;
+	int fd_write, res_write;
+	char *buf[1024];
 
+	/* READ */
+	fd_read = open(file_from, O_RDONLY);
+	if (fd_read < 0)
+		_error(98, file_from);
+
+	/* WRITE */
+	fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_write < 0)
+	{
+		close(fd_read);
+		_error(99, file_to);
+	}
 	do {
-		buf = malloc(sizeof(char) * 1024);
-		if (buf == NULL)
-			_error(98, buf, file_from, fd_read);
-
 		/* READ */
-		fd_read = open(file_from, O_RDONLY);
-		if (fd_read == -1)
-			_error(98, buf, file_from, fd_read);
-
 		res_read = read(fd_read, buf, 1024);
-		if (res_read == -1)
-			_error(98, buf, file_from, fd_read);
+		if (res_read < 0)
+			_error(98, file_from);
 
-		/* Write */
-		fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-		if (fd_write == -1)
-			_error(99, buf, file_to, fd_write);
+		/* WRITE */
+		res_write = write(fd_write, buf, res_read);
+		if (res_write < res_read)
+			_error(99, file_to);
 
-		if (buf != NULL)
-		{
-			lenght = 0;
-			while (*(buf + lenght) != '\0')
-				lenght++;
-			res_write = write(fd_write, buf, lenght);
-			if (res_write == -1 || res_read != res_write)
-				_error(99, buf, file_to, fd_write);
-		}
-		free(buf);
 	} while (res_write == 1024);
-	cl_write = close(fd_write);
-	if (cl_write == -1)
-		_error(100, buf, file_to, fd_write);
-	cl_read = close(fd_read);
-	if (cl_read == -1)
-		_error(100, buf, file_from, fd_read);
+
+	if (close(fd_write) < 0)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", fd_write);
+		exit(100);
+	}
+	if (close(fd_read) < 0)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", fd_read);
+		exit(100);
+	}
 }
 
 /**
@@ -111,8 +100,6 @@ int main(int ac, char **av)
 		exit(99);
 	}
 
-
 	cp(av[1], av[2]);
-
 	return (0);
 }
