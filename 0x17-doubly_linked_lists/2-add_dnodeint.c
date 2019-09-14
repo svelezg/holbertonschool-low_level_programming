63ea4263b483b972b73d06aa33d2a408e73a8e82
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-14 21:14:33 +0000

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,27 +12,25 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *head_node;
 
 	if (head == NULL)
 		return (NULL);
-
-	head_node = *head;
 
 	new_node = malloc(sizeof(dlistint_t)); /* 1. allocate node */
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n; /* 2. put in the data  */
+	new_node->n = n; /* put in the data  */
+	new_node->prev = NULL; /* Make prev of new node as NULL */
 
-	new_node->next = (head_node); /* 3. Make next of new node as head */
+	if (!*head)
+	{	new_node->next =  NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
-	new_node->prev = NULL; /* 4. Make prev of new node as NULL */
-
-	if (head_node != NULL)
-		head_node->prev = new_node; /*5. change prev of head node to new node */
-
-	*head = new_node; /* 6. move the head to point to the new node */
-
-	return (*head);
+	new_node->next = *head; /* Point next of new node to old head */
+	(*head)->prev = new_node; /* change prev of head node to new node */
+	*head = new_node; /* move the head to point to the new node */
+	return (new_node);
 }
