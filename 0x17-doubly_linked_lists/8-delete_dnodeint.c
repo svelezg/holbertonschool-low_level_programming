0f37770452a56fcb48b45fc8511a9fe266926fb0
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-14 21:53:07 +0000

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,42 +12,38 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *node;
-	dlistint_t *d_node;
 	unsigned int i = 0;
 
-	if (*head == NULL || head == NULL)
+	if (!*head)
 		return (-1);
-	d_node = *head; /* Store head node */
-	if (d_node->next == NULL && index == 0)
-	{	free(*head);
-		*head = NULL;
+
+	node = *head;
+
+	while (i < index) /* traverse til node */
+	{
+		if (node->next != NULL)
+		{	node = node->next;
+			i++;
+		}
+		else
+			return (-1);
+	}
+
+	if (!node)
+		return (-1);
+
+	if (index == 0) /* If head node to be deleted (idex == 0) */
+	{	*head = node->next; /* Changed head */
+		if (node->next)
+			node->next->prev = NULL; /* prev of second node to NULL */
+		free(node); /* free old head */
 		return (1);
 	}
 
-	if (d_node != NULL && index == 0) /* If head node to be deleted (idex == 0) */
-	{	*head = d_node->next; /* Changed head */
-		d_node->next->prev = *head; /* prev of second node to head */
-		free(d_node); /* free old head */
-		return (1);
-	}
 
-	while (d_node != NULL && i <= index) /* Search for index to be del, track*/
-	{	node = d_node; /* of previous node as we need to change 'node->next'*/
-		d_node = d_node->next;
-		i++;
-		printf("i= %i (d_node: %i) (node: %i) \n", i, d_node->n, node->n);
-	}
-
-	if (d_node == NULL && i <= index) /* If index exceeds linked list */
-		return (-1);
-
-	if (d_node->next != NULL)
-	{	node->next = d_node->next; /* Unlink (next) the node from linked list*/
-		if (d_node->next != NULL)
-			d_node->next->prev = node; /* Unlik (prev) the node from linked list*/
-	}
-	else
-		*head = NULL;
-	free(d_node); /* Free memory */
+	node->prev->next = node->next; /*jump node forward */
+	if (node->next)
+		node->next->prev = node->prev; /*jump node bakwards */
+	free(node); /* Free memory */
 	return (1);
 }
