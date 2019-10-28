21642a2ad935b43573880dc2fa669cf82ebee4c4
santiagovg <svelezg.velezgarcia@gmail.com>
2019-10-28 10:59:54 -0500

    1-insertion_sort

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -4,11 +4,11 @@
  * insertion_sort_list - sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm.
  *
- * @alist: array
+ * @list: array
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *right, *left, *node_1, *node_2, *node_3;
+	listint_t *right, *left, *node_2, *node_3;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
@@ -16,41 +16,32 @@ void insertion_sort_list(listint_t **list)
 	while (right)
 	{	left = right->prev;
 		while (left)
-		{	node_1 = left;
-			node_2 = left->next;
+		{	node_2 = left->next;
 			node_3 = left->next->next;
-			if (node_1->n > node_2->n)
+			if (left->n > node_2->n)
 			{
-				if (node_1->prev == NULL)
+				if (left->prev == NULL)
 				{			*list = node_2;
-							node_1->next = node_3; /* node_2->next */
-							node_2->next = node_1;
-							node_3->prev = node_1;
+							left->next = node_2->next;
+							node_2->next = left;
+							node_3->prev = left;
 							node_2->prev = NULL; /*node_1->prev;*/
-							node_1->prev = node_2;
+							left->prev = node_2;
 					print_list(*list);
 					break;
 				}
 				else if (node_2->next == NULL)
-				{	node_1->prev->next = node_2;
-					node_1->next = NULL;
-					node_2->next = node_1;
-					node_2->prev = node_1->prev;
-					node_1->prev = node_2;
-					print_list(*list);
-					left = left->prev->prev;
-				}
+					left->next = NULL;
 				else
-					{
-					node_1->prev->next = node_2;
-					node_1->next = node_3; /* node_2->next */
-					node_2->next = node_1;
-					node_3->prev = node_1;
-					node_2->prev = node_1->prev;
-					node_1->prev = node_2;
-					print_list(*list);
-					left = left->prev->prev;
+					{ left->next = node_3; /* node_2->next */
+					node_3->prev = left;
 				}
+				left->prev->next = node_2;
+				node_2->next = left;
+				node_2->prev = left->prev;
+				left->prev = node_2;
+				print_list(*list);
+				left = left->prev->prev;
 			}
 			else
 				break;
