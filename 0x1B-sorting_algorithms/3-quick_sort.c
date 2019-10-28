50862fecf0fc38b3a436f38ebcf2c1f275194a19
santiagovg <svelezg.velezgarcia@gmail.com>
2019-10-28 09:58:37 -0500

    README.md

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ *
+ * @array: array
+ * @size: size
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, p;
+	int temp, min;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		p = i;
+		for (j = 0 + i; j < size; j++)
+		{
+			if (min > array[j])
+			{	min = array[j];
+				p = j;
+			}
+		}
+		if (p != i)
+		{	temp = array[i];
+			array[i] = array[p];
+			array[p] = temp;
+			print_array(array, size);
+		}
+	}
+}
---
+++ 0x1B-sorting_algorithms/README.md
@@ -0,0 +1,15 @@
+# 0x1B-sorting_algorithms
+## 0-bubble_sort.c, 0-O
+## 1-insertion_sort_list.c, 1-O
+## 2-selection_sort.c, 2-O
+## 3-quick_sort.c, 3-O
+## 100-shell_sort.c
+## 101-cocktail_sort_list.c, 101-O
+## 102-counting_sort.c, 102-O
+## 103-merge_sort.c, 103-O
+## 104-heap_sort.c, 104-O
+## 105-radix_sort.c, 105-O
+## 106-bitonic_sort.c, 106-O
+## 107-quick_sort_hoare.c, 107-O
+## 108-sort_deck.c, deck.h
+
