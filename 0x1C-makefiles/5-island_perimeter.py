e17d240e408c8e175f239a6aafc2b1a4cc9c871f
svelezg <svelez.velezgarcia@gmail.com>
2019-11-15 16:23:15 -0500

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,35 +8,35 @@ It supplies one function, island_perimeter.
 def island_perimeter(grid):
     """ island_perimeter function """
     count = 0
-    c_max = len(grid) - 1
-    r_max = len(grid[1]) - 1
+    i_max = len(grid)
+    j_max = len(grid[0])
 
-    for i in range(c_max):
-        for j in range(r_max):
+    for i in range(i_max):
+        for j in range(j_max):
             elem = grid[i][j]
             elem_right = 0
             elem_down = 0
-
-            if j <= c_max:
+            # print("{} {}------------".format(i, j))
+            if j < j_max - 1:
                 elem_right = grid[i][j + 1]
-            if i <= r_max:
+            if i < i_max - 1:
                 elem_down = grid[i + 1][j]
 
-            if (i == 0 or i == r_max) and elem != 0:
+            if (i == 0 or i == i_max - 1) and elem != 0:
                 count = count + 1
                 # print("* up")
-            if (j == 0 or j == c_max) and elem != 0:
+            if (j == 0 or j == j_max - 1) and elem != 0:
                 count = count + 1
                 # print("*left")
 
-            if j != r_max:
+            if j != j_max - 1:
                 if elem != elem_right:
                     count = count + 1
                     # print("*right")
 
-            if j != c_max:
+            if i != i_max - 1:
                 if elem != elem_down:
                     count = count + 1
                     # print("*down")
-            # print("_____________")
+
     return count
