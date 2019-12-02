d5ef0e0201052efcb2cff8fde7b0840596c22c5a
svelezg <svelez.velezgarcia@gmail.com>
2019-12-02 08:40:25 -0500

    Readme and Header files

---
+++ 0x1D-binary_trees/binary_trees.h
@@ -0,0 +1,70 @@
+#ifndef INC_0X1D_BINARY_TREES_BINARY_TREES_H
+#define INC_0X1D_BINARY_TREES_BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
+
+/* 0. New node  */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* 1. Insert left */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* 2. Insert right */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* 3. Delete */
+void binary_tree_delete(binary_tree_t *tree);
+
+/* 4. Is leaf */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* 5. Is root */
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* 6. Pre-order traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* 7. In-order traversal */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* 8. Post-order traversal */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* 9. Height */
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/* 10. Depth */
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+
+#endif
