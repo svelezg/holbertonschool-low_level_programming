75812e6d783a11039bf482e8a1a459047b7b52c2
svelezg <svelez.velezgarcia@gmail.com>
2019-12-04 13:48:16 -0500

    Update Task 18

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,6 +1,36 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_sibling1 - finds the sibling of a node
+ *
+ * @node: pointer to the root node of the tree to traverse
+ * Return: 1 if perfect 0 otherwise
+ */
+
+binary_tree_t *binary_tree_sibling1(binary_tree_t *node)
+
+{
+	int temp;
+
+	if (!node || !node->parent)
+		return (NULL);
+
+	temp = node->n;
+
+	if (node->parent)
+		node = node->parent;
+
+	if (node->left && node->left->n != temp)
+		return (node->left);
+
+	if (node->right && node->right->n != temp)
+		return (node->right);
+
+	return (NULL);
+}
+
+
+/**
  * binary_tree_uncle - finds the uncle of a node
  *
  * @node: pointer to the root node of the tree to traverse
