9e560e635239bda53148478079b8655c83a54041
svelezg <svelez.velezgarcia@gmail.com>
2019-12-04 14:41:18 -0500

    Update Task 101

--- 0x1D-binary_trees/101-binary_tree_levelorder.c
+++ 0x1D-binary_trees/101-binary_tree_levelorder.c
@@ -1,5 +1,44 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height3 - measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: height of a binary tree
+ */
+
+size_t binary_tree_height3(const binary_tree_t *tree)
+{
+	size_t height_l = 0, height_r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+
+	/* height_l = tree->left ? 1 + _height(tree->left) : 0; */
+	if (tree->left)
+		height_l = 1 + binary_tree_height3(tree->left);
+	else
+		height_l = 0;
+
+	/* height_r = tree->right ? 1 + _height(tree->right) : 0; */
+	if (tree->right)
+	{
+		/* printf("right %i\n", tree->right->n); */
+		height_r = 1 + binary_tree_height3(tree->right);
+	}
+
+	else
+		height_r = 0;
+
+	/* return (height_l > height_r ? height_l : height_r); */
+	if (height_l > height_r)
+		return (height_l);
+	else
+		return (height_r);
+
+}
+
 
 /**
  * given_level - nodes at a given level
@@ -40,7 +79,7 @@ void binary_tree_levelorder(const binary_tree_t *tree,
 
 	if (tree != NULL && func != NULL)
 	{
-		height = binary_tree_height(tree);
+		height = binary_tree_height3(tree);
 
 		for (index = 1; index <= height + 1; index++)
 			given_level((binary_tree_t *)tree, index, func);
