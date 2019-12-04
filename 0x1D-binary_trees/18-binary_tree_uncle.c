7e6a3d19782263a3edd2a2af039fee7e5f024b37
svelezg <svelez.velezgarcia@gmail.com>
2019-12-04 13:49:30 -0500

    Update Task 6, 7, 8

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -43,5 +43,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	return (binary_tree_sibling(node->parent));
+	return (binary_tree_sibling1(node->parent));
 }
