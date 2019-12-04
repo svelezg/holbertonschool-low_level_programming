8803d025ce5f1a1303345f5b2512d3ca72528dc0
svelezg <svelez.velezgarcia@gmail.com>
2019-12-04 13:19:06 -0500

    Update Task 7, 8

--- 0x1D-binary_trees/7-binary_tree_inorder.c
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -9,7 +9,7 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL || func == NULL)
+	if (tree != NULL && func != NULL)
 	{
 		if (tree->left)
 			binary_tree_inorder(tree->left, func);
