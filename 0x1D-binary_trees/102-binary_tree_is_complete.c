898a20ca844c9931fa89cfe36a63a2f657090be5
svelezg <svelez.velezgarcia@gmail.com>
2019-12-05 09:17:26 -0500

    Update Task 102

--- 0x1D-binary_trees/102-binary_tree_is_complete.c
+++ 0x1D-binary_trees/102-binary_tree_is_complete.c
@@ -6,6 +6,7 @@
  * @tree: pointer to the root node of the tree to traverse
  * Return: height of a binary tree
  */
+
 size_t binary_tree_height4(const binary_tree_t *tree)
 {
 	size_t height_l = 0, height_r = 0;
@@ -13,29 +14,20 @@ size_t binary_tree_height4(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-
-	/* height_l = tree->left ? 1 + _height(tree->left) : 0; */
 	if (tree->left)
 		height_l = 1 + binary_tree_height4(tree->left);
 	else
 		height_l = 0;
 
-	/* height_r = tree->right ? 1 + _height(tree->right) : 0; */
 	if (tree->right)
-	{
-		/* printf("right %i\n", tree->right->n); */
 		height_r = 1 + binary_tree_height4(tree->right);
-	}
-
 	else
 		height_r = 0;
 
-	/* return (height_l > height_r ? height_l : height_r); */
 	if (height_l > height_r)
 		return (height_l);
 	else
 		return (height_r);
-
 }
 
 /**
@@ -44,29 +36,22 @@ size_t binary_tree_height4(const binary_tree_t *tree)
  * @tree: pointer to the root node of the tree to traverse
  * Return: number of nodes with at least 1 child in a binary tree
  */
+
 int binary_tree_balance2(const binary_tree_t *tree)
 {
-	size_t height_left = 0, height_right = 0;
-	int result;
+	int height_left = 0, height_right = 0;
 
 	if (!tree)
 		return (0);
 
 	if (tree->left)
-	{
-		height_left = 1 + binary_tree_height4(tree->left);
-		/* printf("Height_left from %d: %lu\n", tree->left->n, height_left);*/
-	}
+		height_left = 1 + (int)binary_tree_height4(tree->left);
 
 	if (tree->right)
-	{
-		height_right = 1 + binary_tree_height4(tree->right);
-		/*printf("Height from %d: %lu\n", tree->right->n, height_right);*/
-	}
-	result = height_left - height_right;
-	return (result);
-}
+		height_right = 1 + (int)binary_tree_height4(tree->right);
 
+	return (height_left - height_right);
+}
 
 /**
  * binary_tree_is_complete - checks if a binary tree is complete
