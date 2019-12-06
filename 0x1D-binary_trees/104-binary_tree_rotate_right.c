f7663c8424c20ebbe1c0bf53eb2c4421e8b9ede0
svelezg <svelez.velezgarcia@gmail.com>
2019-12-06 07:59:29 -0500

    Task 104

---
+++ 0x1D-binary_trees/104-binary_tree_rotate_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: pointer to the new root node of the tree once rotated
+ */
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree) {
+	binary_tree_t *P, *B, *A;
+
+	if (!tree)
+		return (NULL);
+
+	A = tree;
+	P = A->parent;
+	B = A->left;
+	A->left = B->right;
+
+	if (B->right)
+		B->right->parent = A;
+
+	B->right = A;
+	A->parent = B;
+	B->parent = P;
+
+	if (P) {
+		if (P->left == A)
+			P->left = B;
+		else
+			P->right = B;
+	}
+	return (B);
+}
